5da4962eacdbfb8029054fb4954de3fd56ba9d34
Dan Zhang <danzhang@hnt254.apng.wharton.upenn.edu>
2014-11-30 15:43:57 -0500

    set up oracle apis for tripsterjs

--- tripsterjs/tripsterjs/app.js
+++ tripsterjs/tripsterjs/app.js
@@ -7,7 +7,26 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var oracle = require('oracle');
+var connectData = {
+    hostname: "tripsterdevdb.c487ba8vtwqs.us-east-1.rds.amazonaws.com",
+    port: 1521,
+    database:"TRIP",
+    user: "zhangb_tripster",
+    password: "iheart550",
+}
+/*
+oracle.connect(connectData, function(err, connection) {
+    if (err) { console.log("Error connecting to db:", err); return; }
 
+    connection.execute("SELECT * FROM USERS", [], function(err, results) {
+        if (err) { console.log("Error executing query:", err); return; }
+
+        console.log(results);
+        connection.close(); // call only when query is finished executing
+    });
+});
+*/
 var app = express();
 
 // view engine setup
